3f8606edbe9854fa27f38f6f1b328855548dc424
Emil Storgaard <est@novicell.dk>
2024-10-03 14:51:48 +0200

    translate

--- src/modules/layout/components/cart-dropdown/index.tsx
+++ src/modules/layout/components/cart-dropdown/index.tsx
@@ -100,7 +100,7 @@ const CartDropdown = ({
             data-testid="nav-cart-dropdown"
           >
             <div className="p-4 flex items-center justify-center">
-              <h3 className="text-large-semi">Cart</h3>
+              <h3 className="text-large-semi">Kurv</h3>
             </div>
             {cartState && cartState.items?.length ? (
               <>
