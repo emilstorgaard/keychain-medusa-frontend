562000ed2341b5dc4143e6e3b8ddc0b1c7291836
Emil Storgaard <est@novicell.dk>
2024-10-07 16:34:50 +0200

    translate

--- src/modules/products/components/product-price/index.tsx
+++ src/modules/products/components/product-price/index.tsx
@@ -35,7 +35,7 @@ export default function ProductPrice({
           "text-ui-fg-interactive": selectedPrice.price_type === "sale",
         })}
       >
-        {!variant && "From "}
+        {!variant && "Fra "}
         <span
           data-testid="product-price"
           data-value={selectedPrice.calculated_price_number}
